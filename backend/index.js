cc3f609aa889abe1a024d92d9dcdfd0784665abe
spe9110 <spencerm365@outlook.fr>
2026-02-03 18:42:02 +0100

    add the login logic success

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 // Enable cors
 app.use(cors({
   credentials: true, 
-  origin: process.env.FRONTEND_URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
